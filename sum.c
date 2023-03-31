0f238112abd378698f8e1afdfdf4dc9765290593
Karime Ochoa <karime8aj@gmail.com>
2023-03-31 14:42:15 -0600

    code

---
+++ sum.c
@@ -0,0 +1,36 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int rank, size;
+    int x0 = 5; // initial value
+    int m = 10; // cycles number
+    int sum = x0; // variable for the cumultive sum
+    int next, prev;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    next = (rank + 1) % size;
+    prev = (rank + size - 1) % size;
+    int i = 0;
+    int error = 0;
+    while (i < m && !error) {
+        MPI_Request request;
+        MPI_Isend(&sum, 1, MPI_INT, next, 0, MPI_COMM_WORLD, &request);
+        MPI_Status status;
+        MPI_Recv(&sum, 1, MPI_INT, prev, 0, MPI_COMM_WORLD, &status);
+        int flag;
+        MPI_Test(&request, &flag, &status);
+        if (!flag) {
+            MPI_Abort(MPI_COMM_WORLD, 1);
+            error = 1;
+        }
+        i++;
+    }
+    if (!error) {
+        printf("Proceso %d: suma acumulativa = %d\n", rank, sum);
+    }
+    MPI_Finalize();
+    return 0;
+}
